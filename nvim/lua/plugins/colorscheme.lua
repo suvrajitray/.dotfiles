929b6d477fbb2845122da4e7f05f235da7ae7153
Suvrajit Ray <suvra@Suvrajits-Mac-mini.local>
2025-02-15 19:55:40 +0530

    colorscheme changes

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,60 @@
+return {
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+  {
+    "tokyonight.nvim",
+    opts = { transparent = true, styles = {
+      sidebars = "transparent",
+      floats = "transparent",
+    } },
+  },
+  {
+    "catppuccin/nvim",
+    lazy = true,
+    name = "catppuccin",
+    opts = {
+      transparent_background = true,
+      integrations = {
+        aerial = true,
+        alpha = true,
+        cmp = true,
+        dashboard = true,
+        flash = true,
+        gitsigns = true,
+        headlines = true,
+        illuminate = true,
+        indent_blankline = { enabled = true },
+        leap = true,
+        lsp_trouble = true,
+        mason = true,
+        markdown = true,
+        mini = true,
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+          },
+        },
+        navic = { enabled = true, custom_bg = "lualine" },
+        neotest = true,
+        neotree = true,
+        noice = true,
+        semantic_tokens = true,
+        telescope = true,
+        treesitter = true,
+        treesitter_context = true,
+        which_key = true,
+      },
+    },
+  },
+  -- Configure LazyVim to load colorscheme.
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-mocha",
+    },
+  },
+}
