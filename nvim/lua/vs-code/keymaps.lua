303311439654ce8b99ecf247520c562dbd41f70e
Suvrajit Ray <suvrajit.ray@gmail.com>
2025-03-17 02:10:14 +0530

    multi cursor configuration for vscode.

--- nvim/lua/vs-code/keymaps.lua
+++ nvim/lua/vs-code/keymaps.lua
@@ -1,4 +1,5 @@
 local vscode = require("vscode")
+local cursors = require("vscode-multi-cursor")
 local keymap = vim.keymap
 local opts = { silent = true, noremap = true }
 
@@ -10,11 +11,30 @@ keymap.set("n", "gi", function()
   vscode.call("editor.action.goToImplementation")
 end, opts)
 
--- undo redo like vscode.
-keymap.set("n", "u", function()
-  vscode.call("undo")
+-- undo like vscode.
+-- keymap.set("n", "u", function()
+--   vscode.call("undo")
+-- end, opts)
+
+-- redo like vscode.
+-- keymap.set("n", "U", function()
+--   vscode.call("redo")
+-- end, opts)
+
+keymap.set("n", "U", "<C-r>")
+
+keymap.set({ "n", "x", "i" }, "<C-d>", function()
+  cursors.addSelectionToNextFindMatch()
 end, opts)
 
-keymap.set("n", "U", function()
-  vscode.call("redo")
+keymap.set("n", "<C-d>", "mciw*:nohl<cr>", {
+  remap = true,
+})
+
+keymap.set({ "n", "x", "i" }, "<CS-d>", function()
+  cursors.addSelectionToPreviousFindMatch()
+end, opts)
+
+keymap.set({ "n", "x", "i" }, "<CS-l>", function()
+  cursors.selectHighlights()
 end, opts)
